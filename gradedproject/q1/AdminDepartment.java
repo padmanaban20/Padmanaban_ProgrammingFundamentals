bedf10bf26691164e1d218b3d811d30709d935b1
padmanaban20 <139556298+padmanaban20@users.noreply.github.com>
2023-07-14 05:30:48 -0700

    Add files via upload

---
+++ gradedproject/q1/AdminDepartment.java
@@ -0,0 +1,23 @@
+package com.gradedproject.q1;
+
+public class AdminDepartment extends SuperDepartment {
+
+	String departmentName() {
+
+		return "Admin Department";
+
+	}
+
+	String getTodaysWork() {
+
+		return "Complete your documents Submission";
+
+	}
+
+	String getWorkDeadline() {
+
+		return "Complete by EOD";
+
+	}
+
+}
